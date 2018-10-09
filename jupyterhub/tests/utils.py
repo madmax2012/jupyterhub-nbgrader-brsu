8888f8a68df991a2b84722c0034ac7dbd593319d
Maximilian Joseph Mensing <mmensi2s@biostore.inf.fh-bonn-rhein-sieg.de>
2018-10-09 10:14:12 +0200

    added dependencies - should fork the origin

---
+++ jupyterhub/tests/utils.py
@@ -0,0 +1,22 @@
+from concurrent.futures import ThreadPoolExecutor
+import requests
+
+class _AsyncRequests:
+    """Wrapper around requests to return a Future from request methods
+
+    A single thread is allocated to avoid blocking the IOLoop thread.
+    """
+    def __init__(self):
+        self.executor = ThreadPoolExecutor(1)
+
+    def __getattr__(self, name):
+        requests_method = getattr(requests, name)
+        return lambda *args, **kwargs: self.executor.submit(requests_method, *args, **kwargs)
+
+# async_requests.get = requests.get returning a Future, etc.
+async_requests = _AsyncRequests()
+
+class AsyncSession(requests.Session):
+    """requests.Session object that runs in the background thread"""
+    def request(self, *args, **kwargs):
+        return async_requests.executor.submit(super().request, *args, **kwargs)
